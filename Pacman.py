20d1c30920f2b928f5de6c1043646503082420ce
IonutGorgos <gorgosionut@yahoo.com>
2016-10-12 13:04:20 +0300

    Pacman

---
+++ Pacman.py
@@ -0,0 +1,4 @@
+class Pacman:
+    def __init__(self):
+        self.score = 0
+        self.start = (7, 6)
--- testPacman.py
+++ testPacman.py
@@ -1,9 +1,28 @@
 import argparse
+from util import manhattanDistance
+
+def make_map(matrix):
+    height = 11
+    width = 19
+
+    harta = [[0 for c in xrange(width)] for r in xrange(height)]
+    for i in xrange(height):
+        harta[i] = matrix[i].split('\n')[0].split(' ')
+    return harta
+
+
 def read_from_file(config_file):
+    data = []
     f = open(config_file, 'r')
     for eachLine in f:
-        print eachLine,
+        data.append(eachLine)
     f.close()
+    return data
+
+
+def print_map(data):
+    for i in data:
+        print i
 
 
 def main():
@@ -14,8 +33,10 @@ def main():
     )
     args = parser.parse_args()
     m = args.matrix_file
-    read_from_file(m)
-
+    data = read_from_file(m)
+    harta = make_map(data)
+    #print_map(harta)
+    print manhattanDistance((3,2),(5, 2))
 
 if __name__ == "__main__":
     main()
